a3235e9e69aa1c1524ce0c5209cc501526854128
PiyushShiv26 <piyushshiv26@gmail.com>
2025-10-08 23:36:55 +0530

    updated the css icon

---
+++ lua/plugins/devicons.lua
@@ -0,0 +1,12 @@
+return {
+  "nvim-tree/nvim-web-devicons",
+  opts = {
+    override = {
+      css = {
+        icon = "",       
+        color = "#264de4", 
+        name = "css",      
+      },
+    },
+  },
+}
