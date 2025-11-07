f0247bec9dbc2ceea0276eae6a925cba22771cba
PiyushShiv26 <piyushshiv26@gmail.com>
2025-11-07 18:57:44 +0530

    fixed the issue where markdown-preview gave an error at the load out

--- lua/plugins/markdown-preview.lua
+++ lua/plugins/markdown-preview.lua
@@ -1,7 +1,10 @@
 return {
-  "iamcco/markdown-preview.nvim",
-  build = function() vim.fn["mkdp#util#install"]() end,
-  ft = { "markdown" },
-  cmd = { "MarkdownPreview", "MarkdownPreviewToggle", "MarkdownPreviewStop" },
-}
+	"iamcco/markdown-preview.nvim",
+	build = function()
+		vim.fn["mkdp#util#install"]()
+	end,
+	ft = { "markdown" },
+	cmd = { "MarkdownPreview", "MarkdownPreviewToggle", "MarkdownPreviewStop" },
 
+	lazy = false,
+}
